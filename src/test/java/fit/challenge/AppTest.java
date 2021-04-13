c8ac3f3d2eace7fe7153a055f7b00c75faff86df
Thiago Silveira <thiago.psilveira@aluno.faculdadeimpacta.com.br>
2021-04-12 21:13:30 -0300

    master - Testes do Calculo de IMC

--- src/test/java/fit/challenge/AppTest.java
+++ src/test/java/fit/challenge/AppTest.java
@@ -1,7 +1,5 @@
 package fit.challenge;
 
-import fit.challenge.ImcCalculator;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,7 +14,7 @@ public class AppTest
     @Test
     public void shouldHaveCalculateAsStaticMethodOfImcCalculatorClass()
     {
-        String resultado = ImcCalculator.calculate(88, 1.77); //28.08
+        String resultado = ImcCalculator.calculate(88.00, 1.77); //28.08
         Assert.assertEquals(resultado, "Sobrepeso");
     }
 
